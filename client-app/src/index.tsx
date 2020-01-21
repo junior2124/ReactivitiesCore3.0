c3e70b48b525090b72676309891312f7fd3a5539
escobede <escobede@LESCOBEDOET450>
2020-01-21 15:27:20 -0600

    End of section 11.

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -6,7 +6,11 @@ import "react-toastify/dist/ReactToastify.min.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
+import "react-widgets/dist/css/react-widgets.css";
 import ScrollToTop from "./app/layout/ScrollToTop";
+import dateFnsLocalizer from "react-widgets-date-fns";
+
+dateFnsLocalizer();
 
 export const history = createBrowserHistory();
 
